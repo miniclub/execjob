46d412d6e1ae66907931418df106f24ad49373a8
Toshihiko Minamoto <minamoto@intersystems.com>
2025-10-28 12:32:25 +0900

    複数ファイルが存在した場合、ファイルごとにexecJobをコールする execJobに複数の要求があった時に順番に処理する

--- server/src/csp/user/execJob.js
+++ server/src/csp/user/execJob.js
@@ -1,19 +1,23 @@
-		/// バッチ処理の実行
-		function execJob(url,log,...args){
+		// 引数の保存領域
+		const savedargs =[];
+		var execStatus=0; // 停止
 
+		/// Websocket通信処理
+		function connect(url,log) {
 			// WebSocketを作成
-			const ws = new WebSocket(url);
+			var ws = new WebSocket(url);
 
 			// ソケットがOpenされたときのイベント処理
 			ws.onopen = function(event) {
+				let params = savedargs.shift()
 				log.textContent = `${log.textContent}log - connected\n`;
 				var files=new Array();
 				// パラメータの送信
-				args.forEach(function(elem,index){
+				params.forEach(function(elem,index){
 					if( elem.constructor.name == "File") {
 						// ファイルオブジェクトの場合、必要な情報をコピーする
 						files.push(elem);
-						args[index]={
+						params[index]={
 								'lastModified': elem.lastModified,
 								'name': elem.name,
 								'size': elem.size,
@@ -22,7 +26,7 @@
 					}
 				}); 
 				// パラメータリストを送信
-				ws.send(JSON.stringify(args)); // パラメータ
+				ws.send(JSON.stringify(params)); // パラメータ
 				// ファイルパラメータがある場合、その内容を送信
 				if( files.length>0) {
 					ws.binaryType="arraybuffer";
@@ -45,5 +49,28 @@
 			// データを受信たときのイベント処理
 			ws.onclose = function(event) {
 				log.textContent = `${log.textContent}log - closed\n`;
+				if( savedargs.length > 0) {
+					// 次のWebsocketに接続
+					setTimeout(function() {
+						log.textContent+=`log - reconnect\n`;
+						connect(url,log);
+					}, 1000);					
+				} else {
+					execStatus = 0;
+				}
 			};
+		}
+
+		/// バッチ処理の実行
+		function execJob(url,log,...args){
+
+			// 引数をスタックに積んで終了
+			savedargs.push(args);
+			// 終了
+			if( execStatus > 0) {
+				return;
+			}
+			execStatus = 1;
+			connect(url,log);
+
 		}
